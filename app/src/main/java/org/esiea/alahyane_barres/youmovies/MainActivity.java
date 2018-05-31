57f68de2ec327502bd74a8d59d44a0656202353f
Alahyane <alahyane@et.esiea.fr>
2018-05-31 08:39:16 +0200

    Continue jvai dodo

--- app/src/main/java/org/esiea/alahyane_barres/youmovies/MainActivity.java
+++ app/src/main/java/org/esiea/alahyane_barres/youmovies/MainActivity.java
@@ -1,13 +1,82 @@
 package org.esiea.alahyane_barres.youmovies;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import static org.esiea.alahyane_barres.youmovies.R.*;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+        setContentView(layout.activity_main);
+
+        TextView tv_dn = (TextView) findViewById(id.Date_now);
+        String now = DateUtils.formatDateTime(getApplicationContext(), (new Date()).getTime(), DateFormat.FULL);
+        tv_dn.setText(now);
+
+        Button btn_bm = (Button) findViewById(id.Best_movie);
+        btn_bm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), getString(string.best_movie_text), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        /*Button btn_hw2 = (Button)findViewById(id.button_text2);
+        btn_hw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Voulez vous vraiment quitter ?")
+                        .setTitle("Quitter")
+                        .setPositiveButton("OUI", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                closeContextMenu();
+                            };
+                   );
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            }
+        } );*/
+
+        final Button movieButton = (Button) findViewById(id.Research_movie);
+        movieButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Movies.class);
+                startActivity(intent);
+            }
+        });
+
+        final Button contactButton = (Button) findViewById(id.Contact);
+        contactButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Contact.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+
+    }}
---
+++ app/src/main/java/org/esiea/alahyane_barres/youmovies/Movies.java
@@ -0,0 +1,15 @@
+package org.esiea.alahyane_barres.youmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.esiea.alahyane_barres.youmovies.R;
+
+public class Movies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies);
+    }
+}
